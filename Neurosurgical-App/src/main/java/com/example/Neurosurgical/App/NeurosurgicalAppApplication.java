07d9d51befae8ac548cf28d1bea83d1e426554ff
CristiFiodorov <cristifv59@gmail.com>
2023-04-03 19:02:57 +0300

    Initial commit

---
+++ Neurosurgical-App/src/main/java/com/example/Neurosurgical/App/NeurosurgicalAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.Neurosurgical.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class NeurosurgicalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeurosurgicalAppApplication.class, args);
+	}
+
+}
---
+++ Neurosurgical-App/src/test/java/com/example/Neurosurgical/App/NeurosurgicalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Neurosurgical.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeurosurgicalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
