da79550aebf24f43d5b731b74fc70127ae092ac1
Jitca Diana <65017893+minion26@users.noreply.github.com>
2023-04-07 02:03:56 +0300

    fisierul uitat

--- Neurosurgical-App/src/main/java/com/example/Neurosurgical/App/repositories/UserRepository.java
+++ Neurosurgical-App/src/main/java/com/example/Neurosurgical/App/repositories/UserRepository.java
@@ -3,10 +3,20 @@ package com.example.Neurosurgical.App.repositories;
 import com.example.Neurosurgical.App.models.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
     @Query(value = "SELECT role FROM USERS WHERE id= :id",nativeQuery = true)
     Integer findRoleById(Long id);
+
+    @Query(value = "SELECT * FROM USERS",nativeQuery = true)
+    List<UserEntity> findalll();
+
+    @Query(value = "SELECT * FROM USERS WHERE email_faculty = :mail",nativeQuery = true)
+    UserEntity findByFacMail(@Param("mail") String mail);
 }
