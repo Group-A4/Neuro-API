1686ce15838167eae0060f6d17845cd6e6257edd
sorodocosmin <61987774+sorodocosmin@users.noreply.github.com>
2023-04-23 13:26:29 +0300

    try_add_questionC

---
+++ Neurosurgical-App/src/main/java/com/example/Neurosurgical/App/controllers/QuestionController.java
@@ -0,0 +1,12 @@
+package com.example.Neurosurgical.App.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/questions")
+
+public class QuestionController {
+
+}
