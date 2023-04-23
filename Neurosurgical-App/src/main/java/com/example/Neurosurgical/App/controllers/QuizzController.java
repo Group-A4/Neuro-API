43c37009a78de4adfd6dfc9ee6332dfbcfa1646a
sorodocosmin <61987774+sorodocosmin@users.noreply.github.com>
2023-04-23 13:36:07 +0300

    try_add_quizzC

---
+++ Neurosurgical-App/src/main/java/com/example/Neurosurgical/App/controllers/QuizzController.java
@@ -0,0 +1,10 @@
+package com.example.Neurosurgical.App.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/quizzes")
+public class QuizzController {
+
+}
