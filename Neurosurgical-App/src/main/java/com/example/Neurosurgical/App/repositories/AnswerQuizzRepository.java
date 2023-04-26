baea23f63b8a48904e95316aaa74d28c38d96128
Gabriel <gabriel.ignat.v09@gmail.com>
2023-04-26 19:39:27 +0300

    deleted junk

--- Neurosurgical-App/src/main/java/com/example/Neurosurgical/App/repositories/AnswerQuizzRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Neurosurgical.App.repositories;
-
-import com.example.Neurosurgical.App.models.entities.AnswerQuizzEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerQuizzRepository extends JpaRepository<AnswerQuizzEntity, Long> {
-
-}
